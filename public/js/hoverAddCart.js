78942a2467a5a97a09ab233e6389d41a914b20c2
sleklere <santiagoleklere@gmail.com>
2022-12-22 19:56:32 -0300

    estilos loadProduct, home, varios

---
+++ public/js/hoverAddCart.js
@@ -0,0 +1,11 @@
+// const buttons = document.querySelectorAll(".add-cart");
+// console.log(buttons);
+
+// buttons.forEach((button) => {
+//   button.addEventListener("mouseover", function (e) {
+//     button.textContent = "$1200";
+//   });
+//   button.addEventListener("mouseout", function (e) {
+//     button.textContent = "Agregar al Pedido";
+//   });
+// });
